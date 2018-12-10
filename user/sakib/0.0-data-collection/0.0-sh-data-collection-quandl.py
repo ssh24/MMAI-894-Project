c23b0d2c593831348d5ad624ac22a49495b856c2
ssh24 <sshasan10@hotmail.com>
2018-12-09 20:07:46 -0500

    Add data collection python notebook

--- user/sakib/0.0-data-collection/0.0-sh-data-collection-quandl.py
+++ user/sakib/0.0-data-collection/0.0-sh-data-collection-quandl.py
@@ -2,16 +2,19 @@
 import os
 
 # import libraries required for reading data from the web
-from pandas_datareader import data as web
+import quandl
 from datetime import date
 
 # get the current working directory
 cwd = os.getcwd()
 
+# set your api key
+API_KEY = os.environ['QUANDL_API_KEY']
+
 # set a start date of 1970 Jan 1st to fetch the stock data from
-start = date(1970, 1, 1)
+start = date(2008, 12, 9)
 # grab the latest date for the end date
-end = date.today()
+end = date(2018, 12, 9)
 
 # set api to be the end of day stock price
 api = "EOD/"
@@ -22,13 +25,13 @@ source = "quandl"
 stocks = ['MSFT', 'IBM', 'AAPL', 'INTC', 'JPM', 'AXP']
 
 # loop through each company, grab the data, display some information and save it in a csv file
-for i in range(0, len(stocks)):
-    stock = api + stocks[i]
+for id in stocks:
+    stock = api + id
 
-    df = web.DataReader(stock, source, start, end)
+    df = quandl.get(stock, start_date=start, end_date=end, api_key=API_KEY)
     df.reset_index(inplace=True)
 
-    print "\n\n****** Printing data information for: " + stocks[i] + " ******"
+    print "\n\n****** Printing data information for: " + id + " ******"
 
     print "\nShape: ", df.shape
 
@@ -41,5 +44,4 @@ for i in range(0, len(stocks)):
     print "\nGet the tail: ", df.tail()
 
     print "\nWrite the data to csv ..."
-    df.to_csv(os.path.join(cwd, 'data', '0.0-sh-data-' + stocks[i] + '.csv'), index=False)
-    
+    df.to_csv(os.path.join(cwd, 'data', '0.0-sh-data-' + id + '.csv'), index=False)
