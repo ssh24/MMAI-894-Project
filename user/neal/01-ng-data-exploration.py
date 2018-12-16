de676e10936a1838bd2dec4063f90f3b63917383
Neal Gilmore <neal.gilmore@gmail.com>
2018-12-16 10:57:14 -0500

    added quandl api connectivity

--- user/neal/01-ng-data-exploration.py
+++ user/neal/01-ng-data-exploration.py
@@ -26,3 +26,34 @@ from sklearn.preprocessing import MinMaxScaler
 import config  # stores API keys
 
 import warnings
+
+# Turn off Depreciation and Future warnings
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# %reset -f # reset environment if needed
+
+# Set the plotting style
+plt.style.use('bmh')
+
+# Set the date range
+now = dt.datetime.now()
+
+start_date = dt.datetime(now.year - 10, now.month, now.day)
+end_date = dt.datetime(now.year, now.month, now.day)
+
+# Import a stock
+stock_symbol = 'JPM'
+
+##########
+# # Quandl API
+##########
+quandl.ApiConfig.api_key = config.quandl_api_key
+df_quandl = quandl.get('EOD/%s' % stock_symbol,
+                       start_date=start_date, end_date=end_date)
+
+# ### Sort DataFrame by date
+#df = df.sort_values('Date')
+print(df_quandl.columns.values)
+
+print(df_quandl.head())
