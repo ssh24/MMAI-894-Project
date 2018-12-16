4acbf2b1d4d24630516fd54424f136d3d53db5ae
Neal Gilmore <neal.gilmore@gmail.com>
2018-12-16 11:49:50 -0500

    load data file

--- user/neal/1.0-ng-data-exploration.py
+++ user/neal/1.0-ng-data-exploration.py
@@ -20,3 +20,19 @@ import warnings
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 # %reset -f
+
+# Set the plotting style
+print(plt.style.available)
+plt.style.use('seaborn-whitegrid')
+
+# Import the training set
+filename = '0.0-sh-data-JPM.csv'
+data = pd.read_csv(filename)
+data.head()
+data.shape
+
+# ### Sort DataFrame by date
+data = data.sort_values('Date')
+
+# Double check the result
+print(data.head())
