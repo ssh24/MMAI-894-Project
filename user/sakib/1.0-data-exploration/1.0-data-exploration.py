ca41cc9d22026d32f4c0f39d6a99b879c70372ab
ssh24 <sshasan10@hotmail.com>
2018-12-16 19:35:39 -0500

    Set up initial framework for exploration

--- user/sakib/1.0-data-exploration/1.0-data-exploration.py
+++ user/sakib/1.0-data-exploration/1.0-data-exploration.py
@@ -5,4 +5,34 @@ import pandas as pd
 
 # environment settings
 cwd = os.getcwd()
-data = os.path.join(cwd, 'data', 'raw-data.csv')
+
+def main():
+    # set working directory
+    setwd(os.path.join(cwd, 'data'))
+
+    # load initial data
+    data = load_data('0.0-sh-data-JPM.csv')
+
+    # get the shape of the data
+    print "Shape of the data: ", get_shape(data)
+
+    # get the list of columns
+    print "List of columns: ", get_columns(data)
+
+# set the current working directory
+def setwd(wd):
+    os.chdir(wd)
+
+# load the dataframe
+def load_data(file_name):
+    return pd.read_csv(file_name)
+
+# return the shape of the dataframe
+def get_shape(data):
+    return data.shape
+
+def get_columns(data):
+    return list(data.columns)
+
+if __name__ == '__main__':
+    main()
