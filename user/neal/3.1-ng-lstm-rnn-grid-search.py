dcffac100272d857b24cde8840669f14a5b34b95
Neal Gilmore <neal.gilmore@gmail.com>
2019-01-02 20:48:34 -0500

    plot training and test data

--- user/neal/3.1-ng-lstm-rnn-grid-search.py
+++ user/neal/3.1-ng-lstm-rnn-grid-search.py
@@ -39,3 +39,31 @@ print(data.shape)
 # All stock data except last 60 days
 data_train = data[:(len(data) - num_days_pred)]
 data_test = data[-num_days_pred:]  # Last n days of stock data
+
+# Plot Training set
+plt.figure(figsize=(12, 6))
+plt.plot(range(data_train.shape[0]), data_train['Adj_Open'])
+plt.xticks(range(0, data_train.shape[0], 251),
+           data_train['Date'].loc[::251], rotation=90)
+plt.title('Daily Stock Price (Adj. Open): JPM [Training Data]')
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Adj. Open Price (USD)', fontsize=14)
+
+plt.savefig('../reports/figures/3.1-ng-training-data-JPM-adj-open.png',
+            bbox_inches='tight',
+            dpi=300)
+print(plt.show())
+
+# Plot Test set
+plt.figure(figsize=(12, 6))
+plt.plot(range(data_test.shape[0]), data_test['Adj_Open'])
+plt.xticks(range(0, data_test.shape[0], num_days_pred - 1),
+           data_test['Date'].loc[::num_days_pred - 1], rotation=90)
+plt.title('Daily Stock Price (Adj. Open): JPM [Test Data]')
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Adj. Open Price (USD)', fontsize=14)
+
+plt.savefig('../reports/figures/3.1-ng-test-data-JPM-adj-open.png',
+            bbox_inches='tight',
+            dpi=300)
+print(plt.show())
