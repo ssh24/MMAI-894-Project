74e4e1d67a787993f4a477684509433c84b16f27
Neal Gilmore <neal.gilmore@gmail.com>
2019-01-06 20:32:20 -0500

    updated extended lstm rnn model

--- user/neal/3.0-ng-extended-lstm-rnn-model.py
+++ user/neal/3.0-ng-extended-lstm-rnn-model.py
@@ -16,22 +16,19 @@ Learning A-Z™: Hands-On Artificial Neural Networks
 # Import required libraries
 import os
 import datetime as dt
-import quandl
 import pandas as pd
 import numpy as np
 import urllib.request
 import json
+import math
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_squared_error
 from keras.models import Sequential  # Initializes the Neural Network
 from keras.layers import Dense, Dropout, LSTM
-from keras.utils.vis_utils import plot_model
-from keras.wrappers.scikit_learn import KerasRegressor
 
-import config  # stores my API keys
+import config as cfg  # stores my API keys
 
 import warnings
 
@@ -68,7 +65,7 @@ epochs = 500
 ##########
 # # Alpha Vantage API
 ##########
-api_key = config.alpha_vantage_api_key
+api_key = cfg.ALPHA_VANTAGE_API_KEY
 
 # Import list of stocks
 # Use 'TSX:' to identify Canadian stocks
@@ -81,11 +78,15 @@ for index in range(0, len(stock_symbols)):
 
     print("\n*********** STOCK SYMBOL: %s ***********\n" % stock_symbol)
 
-    url_string_daily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=full&apikey=%s" % (
+#    url_string_daily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=full&apikey=%s" % (
+#        stock_symbol, api_key)
+    url_string_daily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&outputsize=full&apikey={}".format(
         stock_symbol, api_key)
 
     # Save data to file
-    save_to_file = '../data/processed/3.0-ng-alpha-vantage-intraday-stock-market-data-%s-%s.csv' % (
+    save_data_to_file = '../data/processed/3.0-ng-alpha-vantage-intraday-stock-market-data-{}-{}.csv'.format(
+        stock_symbol, file_date)
+    save_results_to_file = '../data/processed/3.0-ng-lstm-rnn-model-prediction-results-{}-{}.csv'.format(
         stock_symbol, file_date)
 
     # Store date, open, high, low, close, adjusted close, volume, and dividend amount values
@@ -115,47 +116,33 @@ for index in range(0, len(stock_symbols)):
                         float(v['7. dividend amount'])]
             df_alpha.loc[-1, :] = data_row
             df_alpha.index = df_alpha.index + 1
-    print('Data saved to : %s' % save_to_file)
-    df_alpha.to_csv(save_to_file, index=False)
+    print('Data saved to : {}'.format(save_data_to_file))
+    df_alpha.to_csv(save_data_to_file, index=False)
 
     # Load it from the CSV
-    print('This file already exists. Loading data from CSV')
-    df_alpha = pd.read_csv(save_to_file)
+    df_alpha = pd.read_csv(save_data_to_file)
 
     # ### Sort DataFrame by date
     df_alpha = df_alpha.sort_values('Date')
 
     # Filter data to last n years from start_date
-    df_alpha = df_alpha[(df_alpha['Date'] > start_date)]
-
-    # Double check the result
-#    print(df_alpha.head())
-
-#    print(df_alpha.plot(figsize=(12, 6)))
+    df_alpha = df_alpha[(df_alpha['Date'] >= start_date)]
 
     # ### Visualize the Adjusted Close Price
     plt.figure(figsize=(12, 6))
     plt.plot(range(df_alpha.shape[0]), df_alpha['Adjusted Close'])
     plt.xticks(range(0, df_alpha.shape[0], 251),
                df_alpha['Date'].loc[::251], rotation=90)
-    plt.title('Daily Stock Price (Adj Close): %s' % stock_symbol)
+    plt.title('Daily Stock Price (Adj Close): {}'.format(stock_symbol))
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Adj. Close Price', fontsize=14)
 
-    plt.savefig('../reports/figures/3.0-ng-alpha-vantage-daily-stock-market-data-adj-close-price-%s.png' % stock_symbol,
+    plt.savefig('../reports/figures/3.0-ng-alpha-vantage-daily-stock-market-data-adj-close-price-{}.png'.format(stock_symbol),
                 bbox_inches='tight',
                 dpi=300)
     print(plt.show())
 
     # ### Splitting Data into Training and Test
-    # Define price variables for training and testing purposes
-#    open_prices = df_alpha.loc[:, 'Open'].values
-#    close_prices = df_alpha.loc[:, 'Close'].values
-#    adj_close_prices = df_alpha.loc[:, 'Adjusted Close'].values
-#    high_prices = df_alpha.loc[:, 'High'].values
-#    low_prices = df_alpha.loc[:, 'Low'].values
-#    mid_prices = (high_prices + low_prices) / 2.0
-
     # ### NOTE: We can't use train_test_split because it would randomly pick rows and the
     # ### order of rows is critical to our analysis
 
@@ -170,11 +157,11 @@ for index in range(0, len(stock_symbols)):
     plt.xticks(range(0, data_train.shape[0], 251),
                data_train['Date'].loc[::251], rotation=90)
     plt.title(
-        'Daily Stock Price (Adj. Close): %s [Training Data]' % stock_symbol)
+        'Daily Stock Price (Adj. Close): {} [Training Data]'.format(stock_symbol))
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Adj. Close Price', fontsize=14)
 
-    plt.savefig('../reports/figures/3.0-ng-training-data-%s-adj-close-%s.png' % (stock_symbol, file_date),
+    plt.savefig('../reports/figures/3.0-ng-training-data-{}-adj-close-{}.png'.format(stock_symbol, file_date),
                 bbox_inches='tight',
                 dpi=300)
     print(plt.show())
@@ -184,11 +171,12 @@ for index in range(0, len(stock_symbols)):
     plt.plot(range(data_test.shape[0]), data_test['Adjusted Close'])
     plt.xticks(range(0, data_test.shape[0], 5),
                data_test['Date'].loc[::5], rotation=90)
-    plt.title('Daily Stock Price (Adj. Close): %s [Test Data]' % stock_symbol)
+    plt.title(
+        'Daily Stock Price (Adj. Close): {} [Test Data]'.format(stock_symbol))
     plt.xlabel('Date', fontsize=14)
     plt.ylabel('Adj. Close Price', fontsize=14)
 
-    plt.savefig('../reports/figures/3.0-ng-test-data-%s-adj-close-%s.png' % (stock_symbol, file_date),
+    plt.savefig('../reports/figures/3.0-ng-test-data-{}-adj-close-{}.png'.format(stock_symbol, file_date),
                 bbox_inches='tight',
                 dpi=300)
     print(plt.show())
@@ -222,7 +210,7 @@ for index in range(0, len(stock_symbols)):
 
     for i in range(num_days_pred, len(data_train)):
 
-        # append the previous 60 days' stock prices
+        # append the previous n days' stock prices
         X_train.append(training_set_scaled[i - num_days_pred:i, 0])
 
         # predict the stock price on the next day
@@ -288,7 +276,7 @@ for index in range(0, len(stock_symbols)):
     print(0.1 * round((end - start) / 6))
 
     # Add an end of work message
-    os.system('say "your %s model has finished processing"' % stock_symbol)
+    os.system('say "your {} model has finished processing"'.format(stock_symbol))
 
     # Print summary of the neural network architecture
     print(model.summary())
@@ -319,24 +307,62 @@ for index in range(0, len(stock_symbols)):
     # Invert the feature scaling
     predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
+    # Add calculation of differences between prediction and actual - ups and downs
+    df_date = pd.DataFrame(data_test.iloc[:, 0:1].values)
+    df_real = pd.DataFrame(real_stock_price)
+    df_pred = pd.DataFrame(predicted_stock_price)
+
+    df_final = pd.concat([df_date, df_real, df_pred], axis=1)
+    df_final.columns = ['Date', 'Real Price', 'Predicted Price']
+    df_final['Real Change'] = df_final['Real Price'] - \
+        df_final['Real Price'].shift(1)
+    df_final['Real Change'].fillna(0, inplace=True)
+    df_final['Real Direction'] = np.where(
+        df_final['Real Change'] >= 0, 'Up', 'Down')
+    df_final['Predicted Change'] = df_final['Predicted Price'] - \
+        df_final['Predicted Price'].shift(1)
+    df_final['Predicted Change'].fillna(0, inplace=True)
+    df_final['Predicted Direction'] = np.where(
+        df_final['Predicted Change'] >= 0, 'Up', 'Down')
+    df_final['Correct Prediction'] = np.where(
+        (df_final['Real Change'] * df_final['Predicted Change']) >= 0, True, False)
+
+    df_final.to_csv(save_results_to_file, index=False)
+
+    pred_acc = round(df_final['Correct Prediction'].sum(
+    ) / len(df_final['Correct Prediction']) * 100, 2)
+
+    print('\nPrediction Accuracy: {}%'.format(pred_acc))
+
+    # Calculate mean squared error
+    test_MSE = mean_squared_error(
+        real_stock_price, predicted_stock_price, multioutput='raw_values')
+
+    # Calculate root mean squared error
+    test_RMSE = math.sqrt(mean_squared_error(
+        real_stock_price, predicted_stock_price))
+
+    print('\nTest Score: %.2f MSE' % (test_MSE))
+    print('Test Score: %.2f RMSE' % (test_RMSE))
+
     # Visualize the results
     # Visualize only predicted period
     plt.figure(figsize=(12, 6))
     plt.plot(real_stock_price,
              color='red',
-             label='Real %s Stock Price (Last %s Days)' % (stock_symbol, num_days_pred))
+             label='Real {} Stock Price (Last {} Days)'.format(stock_symbol, num_days_pred))
     plt.plot(predicted_stock_price,
              color='blue',
-             label='Predicted %s Stock Price (Last %s Days)' % (stock_symbol, num_days_pred))
-    plt.title('%s Stock Price Prediction (%s Days : %s Epochs)' %
-              (stock_symbol, num_days_pred, epochs))
+             label='Predicted {} Stock Price (Last {} Days)'.format(stock_symbol, num_days_pred))
+    plt.title('{} Stock Price Prediction ({} Days : {} Epochs)'.format(
+        stock_symbol, num_days_pred, epochs))
     plt.xticks(range(0, data_test.shape[0], 5),
                data_test['Date'].loc[::5], rotation=90)
     plt.xlabel('Time')
     plt.ylabel('Stock Price')
     plt.legend()
 
-    plt.savefig('../reports/figures/3.0-ng-lstm-rnn-grid-search-model-%s-last-%s-days-%s-epochs.png' % (stock_symbol, num_days_pred, epochs),
+    plt.savefig('../reports/figures/3.0-ng-lstm-rnn-extended-model-{}-last-{}-days-{}-epochs-{}.png'.format(stock_symbol, num_days_pred, epochs, file_date),
                 bbox_inches='tight',
                 dpi=300)
     print(plt.show())
